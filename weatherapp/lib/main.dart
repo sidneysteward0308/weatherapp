3f1bf71fa8c06b45053d87b05fd4e3b2fbe1e83a
tidalcurrent <sidneysteward@gmail.com>
2024-09-24 18:24:53 -0400

    generate weather method

--- weatherapp/lib/main.dart
+++ weatherapp/lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MaterialApp());
 }
 
+String weather = "";
+int temperature = 0;
+
+void generateWeather() {
+  int temperatureRandomizer = Random().nextInt(15) + 15;
+  int weatherRandomizer = Random().nextInt(2);
+  temperature = temperatureRandomizer;
+  if (weatherRandomizer == 0){
+    weather = "Sunny";
+  } else if (weatherRandomizer == 1) {
+    weather = "Cloudy";
+  } else {
+    weather = "Rainy";
+  }
+}
